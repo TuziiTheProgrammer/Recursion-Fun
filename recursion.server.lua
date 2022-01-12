52aabd8dd2a83f0b1203a3fd16913da70560ebee
TuziiTheProgrammer <razerforest101@gmail.com>
2022-01-12 13:09:14 +0100

    Upadte 1

---
+++ recursion.server.lua
@@ -0,0 +1,22 @@
+
+
+--[[
+
+local function RecursionTest(x)
+
+	for i = x and x or 1, 100 do
+		wait()
+		print("Hi")
+		if i == 100 then
+	
+			print("Recursing")
+
+			RecursionTest()
+		end	
+	end
+end
+RecursionTest()
+
+
+
+--]]
